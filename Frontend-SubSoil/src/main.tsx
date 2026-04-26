aa2c264f2d21d08d872a7f3cd8093a4dc7ce8c3a
Aleksey Baranovskiy <lari.bro@yandex.ru>
2026-04-26 21:39:09 +0300

    add: TanStack Query | 0.2.1

--- Frontend-SubSoil/src/main.tsx
+++ Frontend-SubSoil/src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import ReactDOM from 'react-dom/client'
 import { RouterProvider, createRouter } from '@tanstack/react-router'
 import { routeTree } from './routeTree.gen'
+import { QueryProvider } from './app/providers/QueryProvider/QueryProvider'
 
 const router = createRouter({ routeTree })
 
@@ -16,7 +17,9 @@ if (!rootElement.innerHTML) {
   const root = ReactDOM.createRoot(rootElement)
   root.render(
     <StrictMode>
-      <RouterProvider router={router} />
+      <QueryProvider>
+        <RouterProvider router={router} />
+      </QueryProvider>
     </StrictMode>,
   )
 }
