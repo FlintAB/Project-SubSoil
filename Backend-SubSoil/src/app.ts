aaa27c96ecae182af7b2569e35a18fe064989fd7
Aleksey Baranovskiy <lari.bro@yandex.ru>
2026-06-15 12:38:16 +0300

    prepare for Vercel

--- Backend-SubSoil/src/app.ts
+++ Backend-SubSoil/src/app.ts
@@ -6,19 +6,8 @@ import { wellRouter } from './routes/well.routes'
 const app = express()
 
 app.use(cors())
-
 app.use(express.json())
 
-app.use(
-   '/api/wells',
-   wellRouter
-)
+app.use('/api/wells', wellRouter)
 
-const PORT = 3001
-
-app.listen(PORT, () => {
-
-   console.log(
-      `Server started on ${PORT}`
-   )
-})
+export default app
